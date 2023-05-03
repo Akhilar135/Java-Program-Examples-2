ea39bcaec0c1359c2ca93ed3e64fa7f44c53ebed
Akhila135 <128712546+Akhilar135@users.noreply.github.com>
2023-05-03 14:28:52 +0530

    Add files via upload

---
+++ functionalinterface/FunctionalInterfaceDemo.java
@@ -0,0 +1,24 @@
+package functionalinterface;
+
+@FunctionalInterface
+interface  Program{
+    void showVariable(int a);
+}
+public class FunctionalInterfaceDemo {
+    public static void main(String[] args){
+
+        Program p = (int a)-> System.out.println("show a in lambda expression: "+a);
+        p.showVariable(100);
+
+        Program p1 = new Program() {
+
+            public void showVariable(int a){
+              System.out.println("show a in Anonymous: "+a);  
+                
+            }
+        };
+        p1.showVariable(100);
+
+    }
+    
+}
